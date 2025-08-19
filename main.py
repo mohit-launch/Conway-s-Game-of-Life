4cd920c272fae338c2006014021101e4a1bbc536
mohit-launch <mohit.work1910@gmail.com>
2025-08-19 14:26:44 +0530

    changed cells color for more interactive value

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 COLOR_BG=(10,10,10)
 COLOR_GRID=(40,40,40)
 COLOR_DIE_NEXT=(230,245,34)
-COLOR_ALIVE_NEXT=(255,255,255)
+COLOR_ALIVE_NEXT=(120,245,120)
 
 def update(screen,cells,size,withprogress=False):
     updated_cells=np.zeros((cells.shape[0],cells.shape[1]))
